4040d0d2d1c5b708fe1f53d46f663fc9e0d99be8
Kevin Coker <cubmask@gmail.com>
2022-06-08 09:26:30 +0100

    Initial Commit

---
+++ Micro_Pay/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Micro_Pay
+//
+//  Created by Kevin Coker on 08/06/2022.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Micro_Pay/Micro_PayApp.swift
@@ -0,0 +1,17 @@
+//
+//  Micro_PayApp.swift
+//  Micro_Pay
+//
+//  Created by Kevin Coker on 08/06/2022.
+//
+
+import SwiftUI
+
+@main
+struct Micro_PayApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
